b0dcb017a6a1f5c31453893e5515fd412c65df00
davidcanogallardo <93164111+davidcanogallardo@users.noreply.github.com>
2022-05-02 19:08:08 +0200

    socketio añadido a snap

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server , {  
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// rotacion actual de los ejes del robot
+var positions = {
+    amarillo: 0,
+    rojo: 0,
+    rosa: 0,
+    naranja: 0,
+};
+
+// rotación antigua
+var oldValue = {
+    amarillo: 0,
+    rojo: 0,
+    rosa: 0,
+    naranja: 0,
+};
+
+io.on('connection', (socket) => {
+    socket.emit('updateClient', positions);
+  socket.on('disconnect', () => {
+    // console.log('user disconnected');
+  });
+  socket.on('update', (msg) => {
+    // console.log(msg);
+    positions[msg.color] = msg.pos
+    // Si la diferencia es mayor o menor de 0.2 respecto al valor 
+    // anterior entonces hago un emit para que actualicen el resto de usuario
+    if (oldValue[msg.color]+0.2< positions[msg.color] || oldValue[msg.color]-0.2> positions[msg.color]) {
+        oldValue[msg.color] = positions[msg.color]
+        socket.broadcast.emit('updateClient', positions);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
