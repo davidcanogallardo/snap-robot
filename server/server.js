895e126a2f0dc669e546405980e5717b10226349
David <dcano@ibadia.cat>
2022-05-10 20:32:55 +0200

    snap local o remoto + node server con salas + soporte de las salas en el cliente

--- server/server.js
+++ server/server.js
@@ -10,37 +10,63 @@ const io = new Server(server , {
     }
 });
 
-var positions;
+var defaultPositions;
 // rotacion actual de los ejes del robot
-var positions = {
-  amarillo: {_x:0,_y:0,_z:0},
+var defaultPositions = {
+  amarillo: {_x:0,_y:45,_z:0},
   rojo: {_x:0,_y:0,_z:0},
   rosa: {_x:0,_y:0,_z:0},
   naranja: {_x:0,_y:0,_z:0}
 };
 
+var armPositions = {}
+
 io.on('connection', (socket) => {
-    socket.emit('initialArmPosition', positions);
   socket.on('disconnect', () => {
     // console.log('user disconnected');
   });
 
+  socket.on('leaveRoom', () => {
+    socket.leave(socket.roomId)
+    socket.roomId = null
+  });
+
+  socket.on('joinRoom', (roomId) => {
+    // Lo saca de otra sala si ya estaba
+    if (socket.roomId != null) {
+      console.log("ya tenia una sala antes!");
+      socket.leave(socket.roomId)
+    }
+    // Si es el creador de la sala, crea coordenadas para el robot
+    if (armPositions[roomId] == null) {
+      armPositions[roomId] = JSON.parse(JSON.stringify(defaultPositions))
+    }
+    // Le devuelve las coordenadas
+    io.to(socket.id).emit('initialArmPosition',armPositions[roomId]);
+    // Se une a la sala
+    socket.join(roomId);
+    socket.roomId = roomId
+  })
+
+  socket.on("owo", () => {
+    console.log(`peticion owo ${socket.roomId}`);
+    socket.to(socket.roomId).emit("uwu");
+  })
+
   socket.on('sendRotation', (data) => {
-    socket.broadcast.emit('rotate', data);
+    socket.to(socket.roomId).emit('rotate', data);
+    // socket.broadcast.emit('rotate', data);
   });
 
 
   socket.on('sendArmPosition2', (data) => {
-    socket.broadcast.emit('positionArm2', data);
+    socket.to(socket.roomId).emit('positionArm2', data);
   });
 
-  // socket.on("getArmPositions", (callback) => {
-  //   // enviar las posiciones de verdad
-  //   callback({positions})
-  // });
-  
   socket.on("updateArmRotation", (data) => {
-    positions = data
+    console.log(`room ${socket.roomId} actualiza coords`);
+    armPositions[socket.roomId] = data
+    // defaultPositions = data
   });
   
 });
---
+++ src/classes/Snap.js
@@ -0,0 +1,82 @@
+class Snap {
+    constructor(options, socket) {
+        this.world;
+        this.options = options
+        this.useSocket = options?.useSocket
+        this.socket = socket
+        this.init()
+    }
+
+    init() {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./snap/sw.js');
+        }
+        console.log();
+        this.world = new WorldMorph(document.getElementById('world'));
+        new IDE_Morph().openIn(this.world);
+        
+        // window.loop = this.loop
+        window.world = this.world
+        // this.world.doOneCycle()
+        loop();
+        this.importCustomBlocks()
+        this.enableJS()
+
+        if (this.useSocket == true) {
+            this.connectToSocketRoom();
+        }
+    }
+
+    enableJS() {
+        Process.prototype.enableJS = true
+    }
+    
+    importCustomBlocks() {  
+        var blocks = '<blocks app="Snap! 7, https://snap.berkeley.edu" version="2"><block-definition s="eje1" type="reporter" category="control"><header></header><code></code><translations></translations><inputs></inputs><script><block s="doReport"><l>amarillo</l></block></script></block-definition><block-definition s="eje2" type="reporter" category="control"><header></header><code></code><translations></translations><inputs></inputs><script><block s="doReport"><l>rojo</l></block></script></block-definition><block-definition s="eje3" type="reporter" category="control"><header></header><code></code><translations></translations><inputs></inputs><script><block s="doReport"><l>rosa</l></block></script></block-definition><block-definition s="eje4" type="reporter" category="control"><header></header><code></code><translations></translations><inputs></inputs><script><block s="doReport"><l>naranja</l></block></script></block-definition><block-definition s="posicionar por defecto" type="command" category="control"><header></header><code></code><translations></translations><inputs></inputs><script><block s="doRun"><block s="reportJSFunction"><list></list><l>snap.positionCylinders(0,0,0,0)</l></block><list></list></block></script></block-definition><block-definition s="posicionar ejes: %&apos;eje1&apos; %&apos;eje2&apos; %&apos;eje3&apos; %&apos;eje4&apos;" type="command" category="control"><header></header><code></code><translations></translations><inputs><input type="%n"></input><input type="%n"></input><input type="%n"></input><input type="%n"></input></inputs><script><block s="doRun"><block s="reportJSFunction"><list><l>eje1</l><l>eje2</l><l>eje3</l><l>eje4</l></list><l>snap.positionCylinders(eje1,eje2,eje3,eje4)</l></block><list><block var="eje1"/><block var="eje2"/><block var="eje3"/><block var="eje4"/></list></block></script></block-definition><block-definition s="mover eje %&apos;eje&apos; %&apos;grados&apos; º" type="command" category="control"><header></header><code></code><translations></translations><inputs><input type="%s"></input><input type="%s"></input></inputs><script><block s="doRun"><block s="reportJSFunction"><list><l>eje</l><l>grados</l></list><l>snap.moveCylinder(eje,grados)</l></block><list><block var="eje"/><block var="grados"/></list></block></script></block-definition></blocks>'
+        window.this.openBlocksString(blocks,"Bloques robot",true)
+    }
+
+    positionCylinders(eje1, eje2, eje3, eje4) {
+        console.log(`[snap] posiciono cilindros ${eje1} ${eje2} ${eje3} ${eje4}`);
+        if (this.useSocket) {
+            socket.emit('sendArmPosition2', {arm: "amarillo", degrees: eje1});
+            socket.emit('sendArmPosition2', {arm: "rojo", degrees: eje2});
+            socket.emit('sendArmPosition2', {arm: "rosa", degrees: eje3});
+            socket.emit('sendArmPosition2', {arm: "naranja", degrees: eje4});
+        }
+        arm.positionArm({arm: "amarillo", degrees: eje1})
+        arm.positionArm({arm: "rojo", degrees: eje2})
+        arm.positionArm({arm: "rosa", degrees: eje3})
+        arm.positionArm({arm: "naranja", degrees: eje4})
+    }
+    
+    moveCylinder(color, degrees) {
+        console.log(`[snap] roto cilindro: ${color} ${degrees}º`);
+        if (this.useSocket) {
+            console.log("y a través de socket!");
+            socket.emit('sendRotation', {"arm": color, "degrees": degrees});
+        }
+        arm.newRotation({"arm": color, "degrees": degrees})
+    }
+
+    connectToSocketRoom(roomId) {
+        this.roomId = roomId
+        socket.emit('joinRoom', roomId);
+        this.useSocket = true
+    }
+
+    disconnectFromSocketRoom(){
+        socket.emit("leaveRoom")
+        this.useSocket = false
+    }
+
+    createRoom() {
+        return "#"+(Math.random() + 1).toString(36).substring(6).toUpperCase();
+    }
+
+    updateArmRotation(armRotations) {
+        if (this.useSocket) {
+            socket.emit("updateArmRotation", armRotations)
+        }
+    }
+}
